20e5729183e8f1a57ac756b6fba6b81f714e21f0
uk2ya <ben896@nate.com>
2025-02-10 15:24:07 +0900

    데이터 ENUM 변경으로 item.java 값 변경

--- src/main/java/kr/co/anabada/item/entity/Item.java
+++ src/main/java/kr/co/anabada/item/entity/Item.java
@@ -14,8 +14,8 @@ import lombok.NoArgsConstructor;
 public class Item {
 	private int itemNo;
 	private int userNo;
-	private int cateNo;
-	private int itemAuction;
+	private int itemCate; //ENUM 변경으로 컬럼명 itemCate > itemCate
+	private String itemAuction; //ENUM 변경으로 값 int >String
 	private LocalDateTime itemStart;
 	private LocalDateTime itemEnd;
 	private int itemPrice;
