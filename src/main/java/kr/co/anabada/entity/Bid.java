53c53d2a4ac8c8963028308a4a32072874c19dd8
syeon <oks456@naver.com>
2025-02-10 11:40:27 +0900

    전체 entity 생성

---
+++ src/main/java/kr/co/anabada/entity/Bid.java
@@ -0,0 +1,17 @@
+package kr.co.anabada.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Bid {
+	private int bidNo;
+	private int itemNo;
+	private int userNo;
+	private int bidPrice;
+}
