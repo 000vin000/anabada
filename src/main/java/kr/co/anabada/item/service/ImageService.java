40260b8c88786f47d82785152d8521b13e3a4757
mingmingDD <minji970@naver.com>
2025-02-13 10:59:22 +0900

    itemup 수정, 이미지 여러장 저장 수정

--- src/main/java/kr/co/anabada/item/service/ImageService.java
+++ src/main/java/kr/co/anabada/item/service/ImageService.java
@@ -12,12 +12,14 @@ public class ImageService {
 	@Autowired
 	private ImageMapper imageMapper;
 	
-	public void saveImage(int itemNo, MultipartFile imageFile) throws Exception {
-        if (!imageFile.isEmpty()) {
-            byte[] imageBytes = imageFile.getBytes();
-            imageMapper.save(itemNo, imageBytes); 
-        }
-	}
+	public void saveImage(int itemNo, MultipartFile[] imageFiles) throws Exception {
+		 for (MultipartFile imageFile : imageFiles) {
+	            if (!imageFile.isEmpty()) {
+	                byte[] imageBytes = imageFile.getBytes();
+	                imageMapper.save(itemNo, imageBytes); // 각 이미지 데이터를 DB에 저장
+	            }
+	        }
+	    }
 	
 	public Integer getImageNo(Image image) {
         return image.getImageNo(); 
